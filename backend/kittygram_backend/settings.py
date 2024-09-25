55988c4d68fc66f4b33a10155c116a593383d9be
Anna Arzhakova <arzhakova.anna@yandex.ru>
2024-09-25 13:12:26 +0300

    after review 1_4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,6 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
 
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
